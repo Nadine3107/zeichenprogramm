bb22d0bfa0dd9720602a781e646751ff31c3933c
b0wter <b0wter@gmx.de>
2019-01-24 16:34:57 +0100

    Fix für das Problem, dass die Linien Start/Endpunkte nicht direkt unte rdem Mauscursor erstellt wurden.

--- app.cpp
+++ app.cpp
@@ -72,8 +72,8 @@ void App::onCanvasClicked()
             m_state->setSelectedShape(shape);
         } else if (m_state->selectedTool() == LINE) {
             if(m_lineStarted) {
-                double mStateX = m_state->selectedPosition().rx() - m_state->selectedWidth() / 2;
-                double mStateY = m_state->selectedPosition().ry() - m_state->selectedHeight() / 2;
+                double mStateX = m_state->selectedPosition().rx();
+                double mStateY = m_state->selectedPosition().ry();
                 QBrush brush = QBrush(m_state->selectedColor());
                 brush.setStyle(Qt::BrushStyle::SolidPattern);
                 QPen pen = QPen(brush, m_state->selectedWidth(), Qt::PenStyle::SolidLine, Qt::PenCapStyle::RoundCap, Qt::PenJoinStyle::RoundJoin);
@@ -82,8 +82,8 @@ void App::onCanvasClicked()
                 m_scene->addItem(shape);
                 m_lineStarted = false;
             } else {
-                double mStateX = m_state->selectedPosition().rx() - m_state->selectedWidth() / 2;
-                double mStateY = m_state->selectedPosition().ry() - m_state->selectedHeight() / 2;
+                double mStateX = m_state->selectedPosition().rx();
+                double mStateY = m_state->selectedPosition().ry();
                 m_lineStartX = mStateX;
                 m_lineStartY = mStateY;
                 m_lineStarted = true;
